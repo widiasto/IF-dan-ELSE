5b23ea7be4d6ca7551eee15ec5bc0529252e667e
Gama Widiasto <>
2017-11-15 00:29:31 +0700

    IF dan ELSE
    BELAJAR

---
+++ IF dan ELSE.py
@@ -0,0 +1,20 @@
+nama = "Gama Widiasto"
+nim = "04217017"
+umur = 18
+
+print("Namaku :" + nama)
+print("Nim :" + nim)
+print("Umur")
+print(umur)
+
+if(umur < 35):
+    print("Umurku : ", umur, "tahun -> Masih Muda")
+else:    
+    print("Umurku : ", umur, "tahun -> Sudah Tua")
+
+umur+=20
+
+if(umur <= 35):
+    print("Umurku 20 Tahun ke Depan :", umur, "tahun -> Masih Muda")
+else:
+    print("Umurku 20 Tahun ke Depan :", umur, "tahun -> Sudah Tua")
